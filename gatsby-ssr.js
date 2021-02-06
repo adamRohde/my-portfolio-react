53681bb85fed514efc9c5c54186d5b034e311e44
adamRohde <adamrohde@outlook.com>
2021-02-06 01:39:46 -0600

    new build

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+import Provider, { layoutContext } from "./provider"
+import react, { useState } from "react"
+
+export const wrapRootElement = Provider
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
