82aeeed6e51200cfd66e4b69b4f0c42784ec5958
adamRohde <adamrohde@outlook.com>
2021-02-06 21:09:29 -0600

    clean, rebuild, took out context...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,41 @@
 import React, { useContext } from "react"
 import scrollTo from "gatsby-plugin-smoothscroll"
-import { layoutContext } from "../../provider"
 import Link from "gatsby-link"
 
 const Navbar = () => {
   console.log("Current Navbar page number is ")
 
   return (
-    <layoutContext.Consumer>
-      {context => (
-        <div className="section-nav">
-          {console.log("layout context is ", context.layout)}
-          <div className="container">
-            <div className="navbar-wrapper">
-              <div className="links-wrapper">
-                <button>
-                  <Link
-                    className="nav-links"
-                    to="/"
-                    activeStyle={{ color: "transparent" }}
-                    partiallyActive={true}
-                  >
-                    <h3>Back</h3>
-                  </Link>
-                </button>
+    <div className="section-nav">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <div className="links-wrapper">
+            <button>
+              <Link
+                className="nav-links"
+                to="/"
+                activeStyle={{ color: "transparent" }}
+                partiallyActive={true}
+              >
+                <h3>Back</h3>
+              </Link>
+            </button>
 
-                <div>
-                  <button onClick={() => scrollTo("#header")}>
-                    <h3>About</h3>
-                  </button>
-                  <button onClick={() => scrollTo("#work")}>
-                    <h3>Projects</h3>
-                  </button>
-                  <button onClick={() => scrollTo("#contact")}>
-                    <h3>Contact</h3>
-                  </button>
-                </div>
-              </div>
+            <div>
+              <button onClick={() => scrollTo("#header")}>
+                <h3>About</h3>
+              </button>
+              <button onClick={() => scrollTo("#work")}>
+                <h3>Projects</h3>
+              </button>
+              <button onClick={() => scrollTo("#contact")}>
+                <h3>Contact</h3>
+              </button>
             </div>
           </div>
         </div>
-      )}
-    </layoutContext.Consumer>
+      </div>
+    </div>
   )
 }
 
