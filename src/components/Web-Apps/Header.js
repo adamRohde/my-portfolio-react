6170527a513cface93906a37bb4b1c00f8114212
adamRohde <adamrohde@outlook.com>
2021-02-14 22:25:09 -0600

    refactor on the webapps

---
+++ src/components/Web-Apps/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../../yourdata"
+
+const Header = () => {
+  return (
+    <section className="web-apps-header-wrapper">
+      <h2>Web Apps</h2>
+      <p>
+        These are apps I created to help myself better understand web
+        developement. All of these apps are my own. No tutorials were followed.
+      </p>
+    </section>
+  )
+}
+export default Header
--- src/pages/web-apps.js
+++ src/pages/web-apps.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
 import Layout from "../components/Layout/layout"
 import WebAppsCard from "../components/Web-Apps/web-apps-card"
+import Header from "../components/Web-Apps/Header"
+
 import data from "../yourdata"
 import Fade from "react-reveal/Fade"
 import { layoutContext } from "../../provider"
@@ -14,24 +16,15 @@ const webapps = () => {
           {context => (
             <>
               <div className="section">
-                <div className="container">
+                <div className="container-center">
                   {context.changeLayout("WEB_APPS")}
-                  <div className="web-apps-intro">
-                    <h2>Web Apps</h2>
-                    <p>
-                      These are apps I created to help myself better understand
-                      web developement. All of these apps are my own. No
-                      tutorials were followed.
-                    </p>
-                  </div>
+                  <Header />
                   <WebAppsCard />
                 </div>
               </div>
             </>
           )}
         </layoutContext.Consumer>
-
-        <div style={{ marginBottom: "8rem" }}></div>
       </Layout>
     </>
   )
