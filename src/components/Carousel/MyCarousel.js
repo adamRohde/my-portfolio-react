6efa48ed204a5f05954c918017fee9343ffd2670
adamRohde <adamrohde@outlook.com>
2021-06-01 10:09:51 -0500

    changes

--- src/components/Carousel/MyCarousel.js
+++ src/components/Carousel/MyCarousel.js
@@ -1,31 +1,7 @@
 import React from "react"
 import { Carousel } from "3d-react-carousal"
 import data from "../../yourdata"
-
-// const MyCarousel = () => {
-//   return (
-//     <div className="projects-grid">
-//       {data.mywork.map(mywork => (
-//         <span>
-//           <a
-//             className="project-card"
-//             href={mywork.url ? mywork.url : ""}
-//             target={mywork.target}
-//           >
-//             <div className="project-img">
-//               <Carousel
-//                 slides={mywork.imageSrc}
-//                 autoplay={true}
-//                 interval={1000}
-//               />
-//               <p>{mywork.title}</p>
-//             </div>
-//           </a>
-//         </span>
-//       ))}
-//     </div>
-//   )
-// }
+const { uuid } = require("uuidv4")
 
 let slides = [
   <img src={data.mywork[0].imageSrc} alt="1" />,
@@ -33,17 +9,35 @@ let slides = [
   <img src={data.mywork[2].imageSrc} alt="3" />,
 ]
 
+// const slides = [
+//   {
+//     key: uuid(),
+//     content: <img src={data.mywork[0].imageSrc} alt="1" />,
+//   },
+//   {
+//     key: uuid(),
+//     content: <img src={data.mywork[1].imageSrc} alt="2" />,
+//   },
+//   {
+//     key: uuid(),
+//     content: <img src={data.mywork[2].imageSrc} alt="3" />,
+//   },
+// ]
+
 const MyCarousel = () => {
   return (
     <div>
       <div
         style={{
           transform: "skewY(7deg)",
-          marginLeft: "8vw",
-          marginRight: "8vw",
+          marginLeft: "12vw",
+          marginRight: "12vw",
+          marginTop: "2vh",
+          zIndex: 3,
+          position: "relative",
         }}
       >
-        <Carousel slides={slides} autoplay={false} />
+        <Carousel slides={slides} />
       </div>
     </div>
   )
--- src/components/Layout/Navbar2.js
+++ src/components/Layout/Navbar2.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { layoutContext } from "../../../provider"
 import { Link } from "gatsby"
+import Fade from "react-reveal/Fade"
 
 const Navbar2 = () => {
   console.log("Current Navbar page number is ")
@@ -8,25 +9,27 @@ const Navbar2 = () => {
   return (
     <layoutContext.Consumer>
       {context => (
-        <div className="section-nav-2">
-          {console.log("layout context is ", context.layout)}
-          <div className="navbar-wrapper">
-            <div>
-              <a>
-                <Link to="/random-spanish">randomspanish.com</Link>
-              </a>
-              <a>
-                <Link to="/web-apps">Web Apps</Link>
-              </a>
-              <a>
-                <Link to="/industrial-automation">Industrial Automation</Link>
-              </a>
-              <a>
-                <Link to="/mobile">Mobile Apps</Link>
-              </a>
+        <Fade top cascade>
+          <div className="section-nav-2">
+            {console.log("layout context is ", context.layout)}
+            <div className="navbar-wrapper">
+              <div>
+                <a>
+                  <Link to="/random-spanish">randomspanish.com</Link>
+                </a>
+                <a>
+                  <Link to="/web-apps">Web Apps</Link>
+                </a>
+                <a>
+                  <Link to="/industrial-automation">Industrial Automation</Link>
+                </a>
+                <a>
+                  <Link to="/mobile">Mobile Apps</Link>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Fade>
       )}
     </layoutContext.Consumer>
   )
