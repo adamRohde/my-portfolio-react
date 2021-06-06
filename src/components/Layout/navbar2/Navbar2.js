5599e896f57be18aa85c2be326280f1007385f38
adamRohde <adamrohde@outlook.com>
2021-06-06 14:02:14 -0500

    removed getUrlParts

--- src/components/Layout/navbar2/Navbar2.js
+++ src/components/Layout/navbar2/Navbar2.js
@@ -9,24 +9,24 @@ const Navbar2 = () => {
   let divAnimationClass = "fade-in-image"
   const url = typeof window !== "undefined" ? window.location.href : ""
 
-  address = getUrlParts(url)
+  //address = getUrlParts(url)
 
-  function getUrlParts(url) {
-    var a =
-      document.createElement("a") !== "undefined"
-        ? document.createElement("a")
-        : ""
-    if (a !== "undefined") {
-      a.href = url
-      return {
-        pathname: a.pathname,
-      }
-    } else {
-      a.href = ""
-    }
-  }
+  // function getUrlParts(url) {
+  //   var a =
+  //     document.createElement("a") !== "undefined"
+  //       ? document.createElement("a")
+  //       : ""
+  //   if (a !== "undefined") {
+  //     a.href = url
+  //     return {
+  //       pathname: a.pathname,
+  //     }
+  //   } else {
+  //     a.href = ""
+  //   }
+  // }
 
-  address.pathname === "/portfolio"
+  url === "/portfolio"
     ? (divAnimationClass = "fade-in-image")
     : (divAnimationClass = "")
 
