13fe9f286159d81265603c1d1dd284e9eb3f886f
adamRohde <adamrohde@outlook.com>
2021-06-06 13:41:01 -0500

    conditioned window.location.href

--- src/components/Layout/navbar2/Navbar2.js
+++ src/components/Layout/navbar2/Navbar2.js
@@ -4,59 +4,54 @@ import { Link } from "gatsby"
 import { Navbar, Nav } from "react-bootstrap"
 import Fade from "react-reveal/Fade"
 
-//let currentUrl = window.location.href
-
 const Navbar2 = () => {
-  console.log("Current Navbar page number is ")
-
-  let address = " "
+  let address = ""
   let divAnimationClass = "fade-in-image"
+  const url = typeof window !== "undefined" ? window.location.href : ""
 
-  // function getUrlParts(url) {
-  //   var a = document.createElement("a")
-  //   a.href = url
-  //   return {
-  //     href: a.href,
-  //     host: a.host,
-  //     hostname: a.hostname,
-  //     port: a.port,
-  //     pathname: a.pathname,
-  //     protocol: a.protocol,
-  //     hash: a.hash,
-  //     search: a.search,
-  //   }
-  // }
+  address = getUrlParts(url)
 
-  // console.log("Hello from navbar2 ", currentUrl)
+  function getUrlParts(url) {
+    var a = document.createElement("a")
+    a.href = url
+    return {
+      pathname: a.pathname,
+    }
+  }
 
-  // address = getUrlParts(currentUrl)
-
-  // address.pathname === "/portfolio"
-  //   ? (divAnimationClass = "fade-in-image")
-  //   : (divAnimationClass = "")
+  address.pathname === "/portfolio"
+    ? (divAnimationClass = "fade-in-image")
+    : (divAnimationClass = "")
 
   return (
     <layoutContext.Consumer>
       {context => (
         <div className={divAnimationClass}>
-          <div className="section-nav-2">
-            <div className="navbar-wrapper-2">
-              <div>
-                <Link to="/portfolio/random-spanish" activeClassName="active2">
-                  RandomSpanish.com
-                </Link>
-                <Link to="/portfolio/web-apps" activeClassName="active2">
-                  Web Apps
-                </Link>
-                <Link
-                  to="/portfolio/industrial-automation"
-                  activeClassName="active2"
-                >
-                  Industrial Automation
-                </Link>
-                <Link to="/portfolio/mobile" activeClassName="active2">
-                  Mobile
-                </Link>
+          <div>
+            <div className="section-nav-2">
+              <div className="navbar-wrapper-2">
+                <div>
+                  <Link
+                    to="/portfolio/random-spanish"
+                    activeClassName="active2"
+                    location={address}
+                  >
+                    {console.log("Hello from Link ", address)}
+                    RandomSpanish.com
+                  </Link>
+                  <Link to="/portfolio/web-apps" activeClassName="active2">
+                    Web Apps
+                  </Link>
+                  <Link
+                    to="/portfolio/industrial-automation"
+                    activeClassName="active2"
+                  >
+                    Industrial Automation
+                  </Link>
+                  <Link to="/portfolio/mobile" activeClassName="active2">
+                    Mobile
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
