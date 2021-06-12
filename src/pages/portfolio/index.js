bddaa9f4a7fdf4c7dcfd4b12133325fc39a6ec22
adamRohde <adamrohde@outlook.com>
2021-06-11 19:45:30 -0500

    added all sections to portfolio... just need to add scroll tags

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -7,7 +7,10 @@ import Navbar1 from "../../components/Layout/navbar/Navbar"
 import Navbar2 from "../../components/Layout/navbar2/Navbar2"
 import Footer from "../../components/Layout/Footer"
 import Portfolio from "./Portfolio"
-import { Container } from "semantic-ui-react"
+import RandomSpanish from "./random-spanish/index"
+import WebApps from "./web-apps/index"
+import IndustrialAutomation from "./industrial-automation/index"
+import Mobile from "./mobile/index"
 
 const IndexPage = () => {
   return (
@@ -22,6 +25,10 @@ const IndexPage = () => {
               <Navbar2 />
               <div className="section-main">
                 <Portfolio />
+                <RandomSpanish />
+                <WebApps />
+                <IndustrialAutomation />
+                <Mobile />
               </div>
               <Footer />
             </>
