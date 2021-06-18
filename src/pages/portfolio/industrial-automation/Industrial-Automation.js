b8eb75b10564bd698283cc875bf0f32ebf35b64e
adamRohde <adamrohde@outlook.com>
2021-06-17 22:56:55 -0500

    some formatting to industrial automation section

--- src/pages/portfolio/industrial-automation/Industrial-Automation.js
+++ src/pages/portfolio/industrial-automation/Industrial-Automation.js
@@ -7,14 +7,20 @@ import About from "../../../components/Industrial-Automation/About"
 
 const workprojects = () => {
   return (
-    <section>
+    <>
       {/* <Header title={"Industrial Automation"} /> */}
       <h2>Industrial Automation</h2>
       <div className="ia-grid">
-        <About />
-        <MainImage />
+        <p>
+          For 13 years I have worked in the field of industrial automation.
+          During this time I have written C#, Java and VBA applications. Not to
+          mention to countless PLC programs I have written as well. I don't
+          mention this work as much because I want this site to be more about
+          web applications. Increasily however,these two worlds are starting to
+          meet.
+        </p>
       </div>
-    </section>
+    </>
   )
 }
 
